55b1730bab6b864dfa91d8ba8ed9933b24eff4fe
QEDan <danpmazur@gmail.com>
2023-07-03 18:14:11 -0700

    Added some checks and tests.

--- pytorch_lightning_callback/dlcheck_callback.py
+++ pytorch_lightning_callback/dlcheck_callback.py
@@ -15,8 +15,10 @@ import logging
 import torch
 
 import lightning.pytorch as L
+from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
 
 logger = logging.getLogger("lightning.pytorch.core")
+EPS = 1.0e-32
 
 
 class DLCheckCallback(L.Callback):
@@ -25,9 +27,10 @@ class DLCheckCallback(L.Callback):
         if config is None:
             config = {}
         self.weight_init_eps = config.get('weight_init_eps', 1.0e-6)
-        self.divergence_threshold_min = config.get('divergence_threshold_min', 1.0e3)
-        self.divergence_threshold_max = config.get('divergence_threshold_max', -1.0e3)
-        self.prev_weights = None
+        self.divergence_threshold_min = config.get('divergence_threshold_min', -1.0e4)
+        self.divergence_threshold_max = config.get('divergence_threshold_max', 1.0e4)
+        self.unstable_learning_threshold_min = config.get('unstable_learning_threshold_min', -8.0)
+        self.unstable_learning_threshold_max = config.get('unstable_learning_threshold_max', 1.0)
         self.prev_params = None
         self.neuron_output_history = None
         self.lowest_loss_value = None
@@ -35,6 +38,14 @@ class DLCheckCallback(L.Callback):
         self.model_modules = None
         self.initial_model = None
         self.loss_history = None
+
+    @staticmethod
+    def get_parameter_outputs(model, input_tensor):
+        """Gets output tensors for each parameter."""
+        node_names = get_graph_node_names(model)[0]  # First element are the training node names
+        model_with_outs = create_feature_extractor(model, node_names)
+        outs = model_with_outs(input_tensor)
+        return outs
 
     def check_weight_initialization(self):
         """Check the weight initialization."""
@@ -44,21 +55,22 @@ class DLCheckCallback(L.Callback):
                 std_p = torch.std(param)
                 if std_p < self.weight_init_eps:
                     passed = False
-                    logger.warning(f"\nWarning: Module {m_name} has very small "
+                    logger.warning(f"\nModule {m_name} has very small "
                                    f"weight variance in parameter {i} at initialization. "
-                                   f"std_p={std_p} < {self.weight_init_eps}. ")
+                                   f"std_p={std_p} < {self.weight_init_eps}. "
+                                   f"Check model initialization.")
         return passed
 
     def check_untrained_params(self, model: L.LightningModule) -> bool:
         """Compare model weights with previous epoch."""
         passed = True
         curr_params = [var for var in model.parameters() if var.requires_grad]
-        for i, curr_p, prev_p in enumerate(zip(curr_params, self.prev_params)):
+        for i, (curr_p, prev_p) in enumerate(zip(curr_params, self.prev_params)):
             if torch.equal(curr_p, prev_p):
                 passed = False
                 logging.warning(f"Parameter {i} {curr_p.name} was not updated with training. "
                                 f"Confirm that this layer is properly connected.")
-        self.prev_weights = [copy.deepcopy(p) for p in curr_params]
+        self.prev_params = [copy.deepcopy(p) for p in curr_params]
         return passed
 
     def check_diverging_params(self, model: L.LightningModule) -> bool:
@@ -66,7 +78,9 @@ class DLCheckCallback(L.Callback):
         passed = True
         curr_params = [var for var in model.parameters() if var.requires_grad]
         for i, curr_p in enumerate(curr_params):
-            percentile_25, percentile_75 = torch.quantile(curr_p, torch.tensor([0.25, 0.75]))
+            percentile_25, percentile_75 = torch.quantile(
+                curr_p,
+                torch.tensor([0.25, 0.75], device=curr_p.device))
             if percentile_25 < self.divergence_threshold_min:
                 passed = False
                 logger.warning(f"Parameter {i} {curr_p.name} has 25% of weights "
@@ -77,6 +91,36 @@ class DLCheckCallback(L.Callback):
                 logger.warning(f"Parameter {i} {curr_p.name} has 25% of weights "
                                f"greater than {self.divergence_threshold_max}. Confirm "
                                f"that this layer is learning properly.")
+        return passed
+
+    def check_unstable_learning(self, trainer, model):
+        """Compare ratio of mean absolute update to mean absolute weights"""
+        passed = True
+        # TODO: Support for multiple optimizers (e.g. GANs)
+        lr = trainer.optimizers[0].defaults['lr']
+        curr_params = [p for p in model.parameters() if p.requires_grad]
+        for i, param in enumerate(curr_params):
+            if (param.grad == 0).all():
+                passed = False
+                logging.warning(f"Paramter {i} {param.name} has zero gradients. "
+                                "Check that layer is connected and learning")
+                continue
+            mean_abs_grad = torch.mean(torch.abs(param.grad))
+            mean_abs_val = torch.mean(torch.abs(param.data))
+            if mean_abs_val == 0:
+                # May be recently initialized
+                continue
+            update_ratio = torch.log10(lr * mean_abs_grad / mean_abs_val + EPS)
+            if update_ratio < self.unstable_learning_threshold_min:
+                passed = False
+                logging.warning(f"Parameter {i} {param.name} has an update ratio "
+                                f"less than {self.unstable_learning_threshold_min}. "
+                                f"Learning may be slow. Consider increasing the learning rate.")
+            if update_ratio > self.unstable_learning_threshold_max:
+                passed = False
+                logging.warning(f"Parameter {i} {param.name} has an update ratio "
+                                f"greater than {self.unstable_learning_threshold_max}. "
+                                f"Learning may be unstable. Consider lowering the learning rate.")
         return passed
 
     def init_callback(self, model: L.LightningModule) -> None:
@@ -103,3 +147,5 @@ class DLCheckCallback(L.Callback):
         """Callback at end of training epoch"""
         self.check_untrained_params(pl_module)
         self.check_diverging_params(pl_module)
+        self.check_unstable_learning(trainer, pl_module)
+        self.check_dead_activations()
--- pytorch_lightning_callback/tests/test_dlcheck_callback.py
+++ pytorch_lightning_callback/tests/test_dlcheck_callback.py
@@ -4,8 +4,21 @@ import lightning.pytorch as L
 import pytest
 import torch
 from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision.models.feature_extraction import get_graph_node_names
 
 from pytorch_lightning_callback.dlcheck_callback import DLCheckCallback
+
+
+class DatasetForTests(Dataset):
+    def __init__(self, shape=(8, 28, 28)):
+        self.shape = shape
+
+    def __len__(self):
+        return self.shape[0]
+
+    def __getitem__(self, idx):
+        return torch.rand((1,) + self.shape[1:]), 0
 
 
 class ModelForTests(L.LightningModule):
@@ -31,16 +44,18 @@ class TestDLCheckCallback:
         """Setup tests"""
         self.model = ModelForTests()
         self.callback = DLCheckCallback()
+        self.data_loader = DataLoader(DatasetForTests(), batch_size=4)
         self.trainer = L.Trainer(
             accelerator="auto",
             devices=1,
-            max_epochs=3,
-            callbacks=[self.callback]
-)
+            max_epochs=3
+        )
 
     @pytest.fixture
     def init(self):
         """Initialize the callback"""
+        self.model = ModelForTests()
+        self.callback = DLCheckCallback()
         self.callback.init_callback(self.model)
 
     def test_check_weight_initialization(self, init):
@@ -55,3 +70,44 @@ class TestDLCheckCallback:
         self.callback.init_callback(self.model)
         passed = self.callback.check_weight_initialization()
         assert not passed
+
+    def test_check_untrained_params_fail(self, init):
+        """Test that params are not updated with no training"""
+        passed = self.callback.check_untrained_params(self.model)
+        assert not passed
+
+    def test_check_untrained_params(self, init):
+        """Test that params are updated with training"""
+        self.trainer.fit(self.model, self.data_loader)
+        passed = self.callback.check_untrained_params(self.model)
+        assert passed
+
+    def test_check_diverging_params(self):
+        """Test that freshly initialized model passes diverging parameters check"""
+        passed = self.callback.check_diverging_params(self.model)
+        assert passed
+
+    def test_check_diverging_params_small(self):
+        """Test that model with small parameters fails the diverging parameters check"""
+        for param in self.model.parameters():
+            param.data = 1.0e2 * self.callback.divergence_threshold_min * param.data
+        passed = self.callback.check_diverging_params(self.model)
+        assert not passed
+
+    def test_check_diverging_params_large(self):
+        """Test that model with large parameters fails the diverging parameters check"""
+        for param in self.model.parameters():
+            param.data = 1.0e2 * self.callback.divergence_threshold_max * param.data
+        passed = self.callback.check_diverging_params(self.model)
+        assert not passed
+
+    def test_check_unstable_learning(self, init):
+        """Test that learning is not unstable."""
+        self.trainer.fit(self.model, self.data_loader)
+        passed = self.callback.check_unstable_learning(self.trainer, self.model)
+        assert passed
+
+    def test_get_parameter_outputs(self, init):
+        outs = self.callback.get_parameter_outputs(self.model, self.data_loader.__iter__().__next__()[0])
+        node_names = get_graph_node_names(self.model)[0]
+        assert any(isinstance(outs[nn], torch.Tensor) for nn in node_names)
